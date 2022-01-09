7ea9e681063407a03841a083e8e8c9b3ff572baa
Verbalinsurection <t.stassinopoulos@stass.eu>
2022-01-09 03:24:25 +0100

    TEMP FIX wallet API return empty

--- ravencoin.py
+++ ravencoin.py
@@ -17,8 +17,9 @@ class RvnWallet():
         data_json = api.api_request(cust_url)
         self.__last_error = api.last_error
         if data_json is not None:
-            self.__balance = float(data_json['balance'])
-            return True
+            if 'balance' in data_json:
+                self.__balance = float(data_json['balance'])
+                return True
 
         if self.__last_error is None:
             self.__last_error = 'Can\'t retrieve json result'
